9dd12f8aa5393844b36c92fffd7c581df025f40a
Jakub Habdas <104032948+Pi3gus@users.noreply.github.com>
2023-05-28 19:54:09 +0200

    one round game

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+const singleRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+    if ((playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'rock' && computerSelection === 'scissors')){
+        
+        console.log('You win')
+    }else if (playerSelection === computerSelection){
+        console.log('Draw')
+    }else{
+        console.log('You lose')
+    }
+    return
+};
+
 const getComputerChoice = ()=>{
     const choices = ['rock','paper','scissors']
     const random = Math.floor(Math.random() *3);
     const choice = choices[random];
-    console.log(choice);
+    return choice;
 }
-getComputerChoice();
+
+const computerSelection = getComputerChoice();
+const playerSelection = 'Rock';
+
+console.log(singleRound(playerSelection,computerSelection));
