4569cd2c284aa3716e67721fa049db71a34be394
Jakub Habdas <104032948+Pi3gus@users.noreply.github.com>
2023-05-28 20:45:12 +0200

    sum points and say who win

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
-const singleRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
-    if ((playerSelection === 'scissors' && computerSelection === 'paper') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'rock' && computerSelection === 'scissors')){
-        
-        console.log('You win')
-    }else if (playerSelection === computerSelection){
-        console.log('Draw')
-    }else{
-        console.log('You lose')
+let playerPoints = 0;
+let computerPoints = 0;
+const game = () =>{
+    const playerSelection = 'Rock';    
+    const getComputerChoice = ()=>{
+        const choices = ['rock','paper','scissors']
+        const random = Math.floor(Math.random() *3);
+        const choice = choices[random];
+        return choice;
     }
+    const computerSelection = getComputerChoice();
+    
+    const singleRound = (playerSelection, computerSelection) => {
+        playerSelection = playerSelection.toLowerCase();
+        if ((playerSelection === 'scissors' && computerSelection === 'paper') ||
+            (playerSelection === 'paper' && computerSelection === 'rock') ||
+            (playerSelection === 'rock' && computerSelection === 'scissors')){
+            
+            playerPoints +=1;
+            console.log('You win!')
+            console.log(`Yours points: ${playerPoints} computer points: ${computerPoints}`)
+        }else if (playerSelection === computerSelection){
+            console.log('Draw')
+            console.log(`Yours points: ${playerPoints} computer points: ${computerPoints}`)
+        }else{
+            computerPoints +=1;
+            console.log('You lose')
+            console.log(`Yours points: ${playerPoints} computer points: ${computerPoints}`)
+            
+        }
+        return
+    };
+
+singleRound(playerSelection,computerSelection);
+
     return
-};
-
-const getComputerChoice = ()=>{
-    const choices = ['rock','paper','scissors']
-    const random = Math.floor(Math.random() *3);
-    const choice = choices[random];
-    return choice;
 }
-
-const computerSelection = getComputerChoice();
-const playerSelection = 'Rock';
-
-console.log(singleRound(playerSelection,computerSelection));
+for(let i = 0; i<5; i++){
+    game()
+}
+if(playerPoints > computerPoints){
+    console.log(`You win by ${playerPoints}`)
+}else if(playerPoints< computerPoints){
+    console.log(`You lose by ${computerPoints} points`)
+}else{
+    console.log(`Draw You: ${playerPoints} Computer${computerPoints}`)
+}
